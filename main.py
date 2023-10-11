a9d2c45562dc60c48a27126759f61af3b44ed4da
Dev Gandhi <devgandhi914@gmail.com>
2023-10-11 10:16:54 -0400

    Relocation script works as intended

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+import os
 import time
+import shutil
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
+
+user = "wildwolf"
+
+# extension types
+docs_ext = [".txt", ".doc", ".docx", ".pdf", ".tex", ".wpd", '.rtf', ".odt"]
+music_ext = [".m4a", ".flac", ".mp3", ".wav", ".wma", ".aac", "."]
+pics_ext = [".jpeg", ".gif", ".svg", ".png", ".psd", ".eps", ".tiff", ".raw"]
+vids_ext = [".mp4", ".wmv", ".avi", ".avchd", ".flv", ".mkv", ".webm"]
+
+# Location to store each type of extension
+docs_location = f"/home/{user}/Documents"
+pics_location = f"/home/{user}/Pictures"
+music_location = f"/home/{user}/Music"
+vids_location = f"/home/{user}/Videos"
+desktop_location = f"/home/{user}/Desktop"
 
 # Event handler
 if __name__ == "__main__":
@@ -8,7 +25,8 @@ if __name__ == "__main__":
     ignore_patterns = None
     ignore_directories = False
     case_sensitive = True
-    my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
+    my_event_handler = PatternMatchingEventHandler(
+        patterns, ignore_patterns, ignore_directories, case_sensitive)
 
 # Function for when the file is created in the watch directory
 def on_created(event):
@@ -17,6 +35,22 @@ def on_created(event):
 # Function for when the file is modified in the watch directory
 def on_modified(event):
     print(f"Hey, {event.src_path} has been modified.")
+    
+    # Seperates directory and filname from the event.src_path
+    dir, file_name = os.path.split(event.src_path)
+
+    # Seperates name and extensions from file_name
+    name, ext = os.path.splitext(file_name)
+
+    # Moves files according to their extensions
+    if ext in tuple(docs_ext):
+        shutil.move(event.src_path, docs_location)
+    elif ext in tuple(music_ext):
+        shutil.move(event.src_path, music_location)
+    elif ext in tuple(pics_ext):
+        shutil.move(event.src_path, pics_location)
+    elif ext in tuple(vids_ext):
+        shutil.move(event.src_path, vids_location)
 
 # Function for when the file is deleted in the watch directory
 def on_deleted(event):
@@ -25,6 +59,7 @@ def on_deleted(event):
 # Function for when the file is moved in the watch directory
 def on_moved(event):
     print(f"Hey, {event.src_path} has moved to {event.dest_path}.")
+
 
 # Defining what functions to call under each circumstance
 my_event_handler.on_created = on_created
@@ -35,7 +70,7 @@ my_event_handler.on_deleted = on_deleted
 # Watchdog - observer
 watch_dir = "/home/wildwolf/Downloads"
 watchdog = Observer()
-watchdog.schedule(my_event_handler, path = watch_dir, recursive=True)
+watchdog.schedule(my_event_handler, path=watch_dir, recursive=True)
 
 watchdog.start()
 
